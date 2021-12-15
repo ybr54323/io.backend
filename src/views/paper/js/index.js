6060bd0b2876b92503472e57dbf4fca5199901ed
杨秉瑞 <1005401496@qq.com>
2021-12-15 20:37:42 +0800

    dev

--- src/views/paper/js/index.js
+++ src/views/paper/js/index.js
@@ -1,11 +1,20 @@
 window.onload = function () {
-  var paperTextAread = document.querySelector("#paper");
-//   var axios;
-//   setTimeout(function () {
-//     axios = axios.create({
-//       timeout: 1000,
-//     });
-//   },1000);
+  var paperTextAread = document.querySelector("#paper"),
+    infoIcon = document.querySelector(".icon"),
+    mask = document.querySelector(".mask"),
+    box = document.querySelector(".box");
+
+  infoIcon.addEventListener("click", function () {
+    mask.classList.toggle("mask--active");
+  });
+
+  mask.addEventListener("click", function () {
+    mask.classList.remove("mask--active");
+  });
+  box.addEventListener("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
 
   paperTextAread.addEventListener("input", function (evt) {
     var value = evt.target.innerText;
